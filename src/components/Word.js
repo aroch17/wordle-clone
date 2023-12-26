32da85e48ba06b063fdfcc7c86ce3e45e960204f
Ayush <86850534+aroch17@users.noreply.github.com>
2023-12-26 15:05:03 +0530

    Added more functionality

--- src/components/Word.js
+++ src/components/Word.js
@@ -1,18 +1,17 @@
 import '../styles/Word.css'
 
-export default function Word({ isDisabled }) {
+export default function Word({ number }) {
     function handleKeyPress(e) {
         if (e.code == "Backspace") {
             const { value, name } = e.target
-            const charNumStr = name.split("-")[1]
+            const charNumStr = name.split("-")[3]
             
             if (value.length == 0) {
                 const charNum = parseInt(charNumStr, 10)
                 if (charNum > 1) {
                     const previousSibling = document.querySelector(
-                        `input[name=char-${charNum - 1}]`
+                        `input[name=row-${number}-char-${charNum - 1}]`
                     );
-                    console.log(previousSibling)
                     previousSibling.focus()
                 }
             }
@@ -22,13 +21,13 @@ export default function Word({ isDisabled }) {
     function handleChange(e) {
         const maxLength = 1
         const { value, name } = e.target
-        const charNumStr = name.split("-")[1]
+        const charNumStr = name.split("-")[3]
 
         if (value.length >= maxLength) {
             const charNum = parseInt(charNumStr, 10)
             if (charNum < 5) {
                 const nextSibling = document.querySelector(
-                    `input[name=char-${charNum + 1}]`
+                    `input[name=row-${number}-char-${charNum + 1}]`
                 );
                 nextSibling.focus()
             }
@@ -38,12 +37,12 @@ export default function Word({ isDisabled }) {
     return (
         <>
             <form action="">
-                <div className="char-input-row" style={isDisabled ? {pointerEvents: "none"} : {}}>
-                    <input name="char-1" className="char-input" type="text" maxLength={1} onChange={handleChange} onKeyDown={handleKeyPress} />
-                    <input name="char-2" className="char-input" type="text" maxLength={1} onChange={handleChange} onKeyDown={handleKeyPress} />
-                    <input name="char-3" className="char-input" type="text" maxLength={1} onChange={handleChange} onKeyDown={handleKeyPress} />
-                    <input name="char-4" className="char-input" type="text" maxLength={1} onChange={handleChange} onKeyDown={handleKeyPress} />
-                    <input name="char-5" className="char-input" type="text" maxLength={1} onChange={handleChange} onKeyDown={handleKeyPress} />
+                <div className="char-input-row">
+                    <input name={"row-" + number + "-char-1"} className="char-input" type="text" maxLength={1} onChange={handleChange} onKeyDown={handleKeyPress} />
+                    <input name={"row-" + number + "-char-2"} className="char-input" type="text" maxLength={1} onChange={handleChange} onKeyDown={handleKeyPress} />
+                    <input name={"row-" + number + "-char-3"} className="char-input" type="text" maxLength={1} onChange={handleChange} onKeyDown={handleKeyPress} />
+                    <input name={"row-" + number + "-char-4"} className="char-input" type="text" maxLength={1} onChange={handleChange} onKeyDown={handleKeyPress} />
+                    <input name={"row-" + number + "-char-5"} className="char-input" type="text" maxLength={1} onChange={handleChange} onKeyDown={handleKeyPress} />
                 </div>
             </form>
         </>
--- src/components/WordTable.js
+++ src/components/WordTable.js
@@ -2,14 +2,33 @@ import { useState } from "react"
 import Word from "./Word"
 
 export default function WordTable() {
-    const [disableRow, setDisableRow] = useState(false)
+    const [currentRow, setCurrentRow] = useState(1)
+
+    function handleKeyDown(e) {
+        // TODO: Only disable row if every input filled
+        if (e.code == "Enter") {
+            setCurrentRow(currentRow + 1)
+        }
+    }
+
     return (
         <>
-            <Word isDisabled={false}/>
-            <Word isDisabled={true}/>
-            <Word isDisabled={true}/>
-            <Word isDisabled={true}/>
-            <Word isDisabled={true}/>
+            <div className="row-1" style={currentRow != 1 ? { pointerEvents: "none" } : {}} onKeyDown={handleKeyDown}>
+                <Word number={1}/>
+            </div>
+            <div className="row-2" style={currentRow != 2 ? { pointerEvents: "none" } : {}} onKeyDown={handleKeyDown}>
+                <Word number={2}/>
+            </div>
+            <div className="row-3" style={currentRow != 3 ? { pointerEvents: "none" } : {}} onKeyDown={handleKeyDown}>
+                <Word number={3}/>
+            </div>
+            <div className="row-4" style={currentRow != 4 ? { pointerEvents: "none" } : {}} onKeyDown={handleKeyDown}>
+                <Word number={4}/>
+            </div>
+            <div className="row-5" style={currentRow != 5 ? { pointerEvents: "none" } : {}} onKeyDown={handleKeyDown}>
+                <Word number={5}/>
+            </div>
+
         </>
     )
 }
