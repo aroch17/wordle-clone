384010e773026cf8caf119b00168f67c53066301
Ayush <86850534+aroch17@users.noreply.github.com>
2023-12-27 13:55:07 +0530

    Added logic to check for correct word

--- src/components/WordTable.js
+++ src/components/WordTable.js
@@ -4,6 +4,7 @@ import WordBlock from "./WordBlock"
 
 export default function WordTable() {
     const [currentRow, setCurrentRow] = useState(1)
+    const CORRECT_WORD = "evoke"
 
     function checkRowComplete(rowNum) {
         const row = document.querySelector(`.row-${rowNum}`).childNodes[0]
@@ -23,6 +24,34 @@ export default function WordTable() {
         return true
     }
 
+    function makeWord(rowNum) {
+        const row = document.querySelector(`.row-${rowNum}`).childNodes[0]
+        let guessed_word = ""
+        for (let i = 0; i < MAX_NUMBER_OF_CHARS; i++) {
+            const ch = row.childNodes[i].value
+            guessed_word += (ch.toUpperCase())
+        }
+        return guessed_word
+    }
+
+    function addGreenBackground(rowNum, charNum) {
+        const char = document.querySelector(
+            `input[name=row-${rowNum}-char-${charNum}]`
+        );
+        char.classList.add("green")
+    }
+
+    function checkWord(rowNum, guessed_word) {
+        if (guessed_word === CORRECT_WORD.toUpperCase()) { 
+            for (let i = 1; i <= MAX_NUMBER_OF_CHARS; i++) {
+                addGreenBackground(rowNum, i)
+            }
+        }
+        console.log(CORRECT_WORD.split(""))
+    }
+
+
+
     function moveNextRow() {
         setCurrentRow(currentRow + 1)
         if (currentRow < MAX_NUMBER_OF_TRIES) {
@@ -35,10 +64,14 @@ export default function WordTable() {
         if (e.code === "Enter") {
             const { name } = e.target
             const rowNum = name.split("-")[1]
-            
-            if (!checkRowComplete(rowNum)) console.log("Not enough letters")
-            else if (!checkRowAlpha(rowNum)) console.log("Invalid characters")
-            else moveNextRow()
+
+            if (!checkRowComplete(rowNum)) { console.log("Not enough letters") }
+            else if (!checkRowAlpha(rowNum)) { console.log("Invalid characters") }
+            else {
+                const guessed_word = makeWord(rowNum)
+                checkWord(1, guessed_word)
+                moveNextRow()
+            }
             // TODO: after last try, focus on leader board button
         }
     }
